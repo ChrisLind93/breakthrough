f219364734cf7dee70f4b454c2fb60fbaf71193d
YoloLigeLidtKodeDer <clind_14@hotmail.com>
2016-02-16 10:52:13 +0100

    first commit

---
+++ Assets/scripts/ballcontroller.js
@@ -0,0 +1,40 @@
+﻿var speed:float = 6000;
+var ball:GameObject;
+var block:GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	if (transform.position.y < -100) {
+		gamecontroller.lives--;
+
+		transform.position.y = GameObject.FindGameObjectWithTag("paddle").transform.position.y+5.5;
+		transform.position.y = GameObject.FindGameObjectWithTag("paddle").transform.position.x;
+		ball.GetComponent.<Rigidbody>().Sleep();
+
+		var pcontroller:paddlecontroller;
+
+		pcontroller = GameObject.FindGameObjectWithTag("paddle").GetComponent(paddlecontroller);
+		pcontroller.attachedBall = this.gameObject;
+	}
+
+	if (gamecontroller.lives == 0) {
+		Application.LoadLevel("gameover");
+	}
+
+	if (GameObject.FindGameObjectsWithTag("block").Length < 1) {
+		Application.LoadLevel("youwon");
+	}
+}
+
+function OnCollisionEnter (col:Collision) {
+	if (col.gameObject.tag == "block") {
+		Destroy(col.gameObject);
+
+		if (gamecontroller.score >=0) {
+			gamecontroller.score = gamecontroller.score + 1;
+		}
+	}
+}
---
+++ Assets/scripts/gamecontroller.js
@@ -0,0 +1,30 @@
+﻿var block:GameObject;
+static var score:int = 0;
+static var lives:int = 3;
+
+function Start () {
+	createBlocks();
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	GUI.color = Color.white;
+	GUI.Label (Rect(300,15,100,50), "Score: " + score);
+
+	GUI.color = Color.white;
+	GUI.Label (Rect(1200,15,100,50), "Lives: " + lives);
+}
+
+
+function createBlocks() {
+	for (var i:int = -5;i<6;i++) {
+		Instantiate(block,Vector3(i*16, 10,0), Quaternion.identity);
+	}
+
+	for (var j:int = -5;j<6;j++) {
+		Instantiate(block,Vector3(j*16, 15,0), Quaternion.identity);
+	}
+}
